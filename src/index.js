cb01a3c212cd2be41a401464f89b10b2f7857650
AlisonA-88 <alison.aird@gmail.com>
2026-01-24 16:36:10 +0000

    Removed console logs

--- src/index.js
+++ src/index.js
@@ -21,10 +21,6 @@ function generatePoem(event) {
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class="generating">  Generating your poem about ${instructionsInput.value} ✍️ </div>`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
-
   axios.get(apiUrl).then(displayPoem);
 }
 
